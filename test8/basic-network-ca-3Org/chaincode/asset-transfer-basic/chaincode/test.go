aee90ee99e676b809f6f744c7cea134adb4bcedf
Joseph Shu <86043952+Shumay14@users.noreply.github.com>
2022-08-25 11:07:19 +0900

    Update test.go

--- test8/basic-network-ca-3Org/chaincode/asset-transfer-basic/chaincode/test.go
+++ test8/basic-network-ca-3Org/chaincode/asset-transfer-basic/chaincode/test.go
@@ -6,4 +6,44 @@ import (
 
 )
 
-// Bond 
+// SmartContract provides functions for managing an Asset
+type SmartContract struct {
+	contractapi.Contract
+}
+
+// Define Data Structure
+// describes ESGBond details
+type ESGBond struct {
+	Issuers		  string	`json:"issuers"`
+	MaxIssuersNum int		`json:"maxissuersnum"`
+	BondNum		  int		`json:"bondnum"`
+	MaxBondNum	  int		`json:maxbondnum`
+	FaceValue 	  int		`json:fv`
+	PresentValue  int		`json:pv`
+	DiscountRate  int		`json:discountrate`
+	IssueDate	  string	`json:issuedate`
+	Maturity      int		`json:maturity`
+	Investor	  string	`json:investor`
+}
+
+// describes Corporate details
+type Corporate struct {
+	Name		  string
+	ID			  string	
+	BondIssue 	  string
+	Balance		  int
+}
+
+// describe Investor details
+type Investor struct {
+	Name		  string
+	ID			  string
+	OwnedBond	  string
+	Balance		  int
+
+}
+
+
+
+
+
