8209576215efe9460fb8cc4afd57bb5c7ff482ce
Joseph Shu <86043952+Shumay14@users.noreply.github.com>
2022-08-25 14:12:49 +0900

    Update test.go

--- test8/basic-network-ca-3Org/chaincode/asset-transfer-basic/chaincode/test.go
+++ test8/basic-network-ca-3Org/chaincode/asset-transfer-basic/chaincode/test.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-
 )
 
 // SmartContract provides functions for managing an Asset
@@ -15,90 +14,97 @@ type SmartContract struct {
 // Define Data Structure
 // describes ESGBond details
 type ESGBond struct {
-	Issuers		  string	`json:"issuers"`
-	MaxIssuersNum int		`json:"maxissuersnum"`
-	BondNum		  int		`json:"bondnum"`
-	MaxBondNum	  int		`json:maxbondnum`
-	FaceValue 	  int		`json:fv`
-	PresentValue  int		`json:pv`
-	DiscountRate  int		`json:discountrate`
-	IssueDate	  string	`json:issuedate`
-	Maturity      int		`json:maturity`
-	Investor	  string	`json:investor`
+	Issuers       string `json:"issuers"`
+	MaxIssuersNum int    `json:"maxissuersnum"`
+	BondNum       int    `json:"bondnum"`
+	MaxBondNum    int    `json:maxbondnum`
+	FaceValue     int    `json:fv`
+	PresentValue  int    `json:pv`
+	DiscountRate  int    `json:discountrate`
+	IssueDate     string `json:issuedate`
+	Maturity      int    `json:maturity`
+	Investor      string `json:investor`
 }
 
 // describes Corporate details
 type Corporate struct {
-	Name		  string	`json:name`
-	ID			  string	`json:id`
-	BondIssue 	  string	`json:bondissue`
-	Balance		  int		`json:balance`
+	Name      string `json:name`
+	ID        string `json:id`
+	BondIssue string `json:bondissue`
+	Balance   int    `json:balance`
 }
 
 // describe Investor details
 type Investor struct {
-	Name		  string 	`json:name`
-	ID			  string	`json:id`
-	OwnedBond	  string	`json:ownedbond`
-	Balance		  int		`json:balance`
+	Name      string `json:name`
+	ID        string `json:id`
+	OwnedBond string `json:ownedbond`
+	Balance   int    `json:balance`
 }
 
-
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	esgbond := []ESGBond{
+	esgbonds := []ESGBond{
 		{Issuers: "", MaxIssuersNum: "", BondNum: "", MaxBondNum: "", FaceValue: "", PresentValue: "", DiscountRate: "", IssueDate: "", Maturity: "", Investor: ""},
 	}
 
-	for 
-	
-}
-
-
-// CC functions for corporates 
-func (s *SmartContract) JoinIssuer(ctx contractapi.TransactionContextInterface) {
-
-}
-
-
-func (s *SmartContract) IssueBond(ctx contractapi.TransactionContextInterface, Issuers string, MaxIssuersNum int, BondNum int, MaxBondNum int, 
-	FaceValue int, PresentValue int, DiscountRate int, IssueDate string, Maturity int, Investor string) error {
-		exists, err := s.AssetExists(ctx, id)
+	for _, esgbond := range esgbonds {
+		esgbondJSON, err := json.Marshal(esgbond)
 		if err != nil {
 			return err
 		}
-		if exists {
-			return fmt.Errorf("the asset %s already exists", id)
+
+		err = ctx.GetStub().PutState(esgbond.Issuers, esgbondJSON)
+		if err != nil {
+			return fmt.Errorf("failed to put to world state. %v", err)
 		}
 
-		
 	}
+	return nil
 
-
-func (s *SmartContract) RedeemBond(ctx contractapi, TransactionContextInterface, BondNum int, OwnedInvestor string) {
-	return 
 }
 
-func (s *SmartContract) ReceivableBondOffering(ctx contractapi.TransactionContextInterface, )
+// CC functions for corporates
+// Issue new Bond
+func (s *SmartContract) IssueBond(ctx contractapi.TransactionContextInterface, Issuers string, MaxIssuersNum int, BondNum int, MaxBondNum int,
+	FaceValue int, PresentValue int, DiscountRate int, IssueDate string, Maturity int, Investor string) error {
+	exists, err := s.AssetExists(ctx, id)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the asset %s already exists", id)
+	}
 
+	esgbond := EsgBond{
+		Issuers:       issuers,
+		MaxIssuersNum: maxissuersnum,
+		BondNum:       bondnum,
+		MaxBondNum:    maxbondnum,
+		FaceValue:     fv,
+		PresentValue:  pv,
+		DiscountRate:  discountrate,
+		IssueDate:     issuedate,
+		Maturity:      maturity,
+		Investor:      investor,
+	}
 
+}
 
+func (s *SmartContract) RedeemBond(ctx contractapi, TransactionContextInterface, BondNum int, OwnedInvestor string) {
+	return
+}
+
+func (s *SmartContract) ReceivableBondOffering(ctx contractapi.TransactionContextInterface)
 
 // CC functions for investors
 func (s *SmartContract) DepositAsset(ctx contractapi.TransactionContextInterface) {}
 
-
 func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterface) {}
-
 
 func (s *SmartContract) WithdrawAsset(ctx contractapi.TransactionContextInterface) {}
 
-
 func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface) {}
-
-
 
 func (s *SmartContract) ListBond(ctx contractapi.TransactionContextInterface, id string) {
 
 }
-
-
